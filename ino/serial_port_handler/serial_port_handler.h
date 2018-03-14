9fb9a0cec46bc9fe5eb1113b9dc8abf62def0211
AlexKaravaev <alexkaravev@gmail.com>
2018-03-14 15:07:21 +0300

    serial port handler updated

--- ino/serial_port_handler/serial_port_handler.h
+++ ino/serial_port_handler/serial_port_handler.h
@@ -1,78 +1,110 @@
-
+#include <stdlib.h>
 #ifndef SERIAL_PORT_HANDLER_H
 #define SERIAL_PORT_HANDLER_H
 
-template <uint8_t size_of_msg,uint8_t n_of_fields>
+
+template <uint8_t size_of_msg,uint8_t n_of_fields,HardwareSerial* serial>
 class serial_handler{
 
 
   uint8_t msg_buffer[size_of_msg];
   //the last element of array, that was added to buffer
   uint32_t last_access;
-  //byte that we need to save, if buffer is already full
-  uint8_t lost_byte;
+  uint32_t speed;
+  uint8_t max_float_size;
 
   public:
-    serial_handler(const int &speed):
-    last_access(0)
+    serial_handler(const int &def_speed, const int &float_size):
+    last_access(0), speed(def_speed), max_float_size(float_size)
     {
-      Serial.begin(speed);
       this->reset_buffer();
     }
 
     ~serial_handler(){
-      Serial.end();
+      serial->end();
+    }
+
+    void init(void){
+      serial->begin(this->speed);
     }
 
 
-
     void reset_buffer(void){
-      for (uint8_t i = 0;i<size_of_msg;++i){
+      for (uint8_t i = 0; i < size_of_msg; ++i){
         this->msg_buffer[i] = -1;
       }
 
     }
 
     bool buffer_update(void){
-      uint8_t incoming_byte = Serial.read();
-      if(incoming_byte == -1){
-        return (this->last_access < size_of_msg) ? false : true;
-      }
-      //if last append indx was < size_of_msg, then append msg to buf
-      // and return false, because buffer not full
-      if(this->last_access < size_of_msg){
+      uint8_t incoming_byte = serial->read();
 
-        this->msg_buffer[last_access] = incoming_byte;
-        ++last_access;
+      if(incoming_byte == -1){
+        return (this->msg_buffer[this->last_access] == '>') ? true : false;
+      }
+
+      this->msg_buffer[this->last_access] = incoming_byte;
+      this->last_access++;
+
+      // if not end of msg return false
+      if(incoming_byte!='>'){
         return false;
+
       }
 
       return true;
 
     }
 
-    float* get_msg(void){
+    double* get_msg(void){
       uint8_t temp_buff[size_of_msg];
-      temp_buff = msg_buffer;
+      memcpy(&temp_buff, &msg_buffer, sizeof(temp_buff));
       this->reset_buffer();
-      this->last_access = 0;
       return this->parse(temp_buff);
     }
 
+
     //parse bytes in buffer(and skip first, last, and separator bytes) and return array of floats
-    float* parse(const uint8_t buf[size_of_msg]){
-      float* answer[n_of_fields];
-      uint8_t first_part[] = {buf[1],buf[2]};
-      uint8_t second_part[] = {buf[4],buf[5]};
-      for (uint8_t i=0; i<n_of_fields; i+=3){
-        uint8_t temp[] = {buf[i+1],buf[i+2]};
-        memcpy(&answer[i],&temp,sizeof(answer[i]));
+    double* parse(const uint8_t buf[size_of_msg]){
+      serial->flush();
+      static double answer[n_of_fields];
+      char char_buff[this->last_access - 1];
+      char value[this->max_float_size];
+      uint8_t counter;
+      uint8_t counter_for_data = 0;
+
+      for(uint8_t i = 0; i < max_float_size; i++){
+        value[i] = '0';
       }
+
+      for(uint8_t i = 0; i < this->last_access; ++i){
+        char_buff[i] = buf[i];
+      }
+
+      for (uint8_t i = 0; i < this->last_access; ++i){
+        if(char_buff[i] == '<')
+          continue;
+        counter = 0;
+        while(char_buff[i] != ';' && char_buff[i] != '>')
+        {
+          if(char_buff[i] != ' ' && char_buff[i] != '\n'){
+            value[counter] = char_buff[i];
+            counter++;
+          }
+          i++;
+        }
+        answer[counter_for_data++] = strtod(value,NULL);
+        for(uint8_t i=0; i < max_float_size; i++){
+          value[i] = '0';
+        }
+        if(char_buff[i] == '>'){
+          break;
+        }
+      }
+      this->last_access = 0;
+
       return answer;
     }
-
-
-
 };
 
 #endif
