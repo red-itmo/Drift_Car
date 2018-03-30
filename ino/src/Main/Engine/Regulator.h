a01a09f33ef1c12bcf64972bb289b3159fd3e76b
RamiNaim <rami.naim2010@yandex.ru>
2018-03-30 21:10:02 +0300

    added draft class for EngineRegulator

--- ino/src/Main/Engine/Regulator.h
+++ ino/src/Main/Engine/Regulator.h
@@ -1,8 +1,36 @@
 #ifndef Regulator_h
 #define Regulator_h
 
+// TODO: think about implementing EngineRegulator as static class, rewrite compute_v callback function
 
-const double d_alpha, Kp = 2000.0;
+// class EngineRegulator holds important values for encoders disk, koefficents
+// and computes output value for engine, and current velocity
+
+class EngineRegulator {
+private:
+    unsigned float d_alpha;
+    float Kp;
+    float des_vel;
+
+public:
+    // args: angle of one hole of disk, propotional koeff and desired velocity
+    EngineRegulator(unsigned float angle, float koeff, float desiredVelocity = 0){
+        d_alpha = angle;
+        Kp = koeff;
+        des_vel = desiredVelocity;
+    }
+
+    void setDesVel(float desiredVelocity){
+        des_vel = desiredVelosity;
+    }
+
+
+    long get_Up(long des_vel){
+        long e = des_vel - trans_vel;
+        return Kp * e;
+    }
+};
+
 unsigned long current_time, time, dt, trans_vel;
 
 void compute_v(){
@@ -15,9 +43,6 @@ void compute_v(){
 	interrupts();
 }
 
-long get_Up(long des_vel){
-  long e = des_vel - trans_vel;
-  return Kp * e;
-}
+
 
 #endif
